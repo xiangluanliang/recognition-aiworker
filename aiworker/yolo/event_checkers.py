5c39ea6bf628c6227218ac69dd57389ff4f3fc7a
Jiale531 <1285570257@qq.com>
2025-07-16 17:15:13 +0800

    跟新摔倒判断和分数计算

--- aiworker/yolo/event_checkers.py
+++ aiworker/yolo/event_checkers.py
@@ -21,32 +21,60 @@ def is_likely_horizontal(bbox, threshold=1.2):
     return (width / height) > threshold
 
 def check_fall(pid, kpts, bbox, person_fall_status, base_angle_thresh, window_size, cooldown_frames):
+
+    # -----  条件1：关键角度
+
+    # 计算关键角度
     mid_shoulder = (kpts[5] + kpts[6]) / 2
     mid_hip = (kpts[11] + kpts[12]) / 2
+
     angle = _angle_between_points(kpts[0], mid_shoulder, mid_hip)
 
-    # 判断 bbox 是否接近水平
+    # -----  条件2：肩宽比
+
+    # 宽高比计算
+    x1, y1, x2, y2 = bbox
+    width = x2 - x1
+    height = y2 - y1
+    wh_ratio = width / (height + 1e-5)
+
+    # -----  条件3：姿势水平情况
+
+    # 判断姿势是否“水平”
     is_horizontal = is_likely_horizontal(bbox)
     angle_thresh = base_angle_thresh + 15 if is_horizontal else base_angle_thresh
 
-    # 计算评分（角度 & bbox宽高比）
-    angle_score = np.clip((base_angle_thresh - angle) / base_angle_thresh * 40, 0, 40)
-    if angle < 10:  # 极端角度，给满分
-        angle_score = 40
-    wh_ratio = (bbox[2] - bbox[0]) / (bbox[3] - bbox[1] + 1e-5)
-    wh_score = np.clip((wh_ratio - 1.2) / 1.8 * 30, 0, 30)
+    # -----  条件4：高度变化检测
+    status = person_fall_status[pid]
+    if 'prev_height' not in status:
+        status['prev_height'] = height
+    height_change = abs(height - status['prev_height']) / (status['prev_height'] + 1e-5)
+    status['prev_height'] = height
+
+    # -----  分数计算
+
+    # 角度得分（0-30）
+    angle_score = 30 if angle < 10 else np.clip((angle_thresh - angle) / angle_thresh * 30, 0, 30)
+    # 宽高比得分（0-50）
+    wh_score = np.clip((wh_ratio - 1.2) / 1.8 * 40, 0, 40)
+    # 高度变化分数
+    motion_score = np.clip(height_change / 0.5 * 10, 0, 10)
+    # 动作持续得分（0-20）
+    status = person_fall_status[pid]
+    duration_score = np.clip(status['fall_frame_count'] / window_size * 20, 0, 20)
+
+    total_score = (angle_score + wh_score + duration_score + motion_score )/100
 
     status = person_fall_status[pid]
-
-    motion_score = np.clip(status['fall_frame_count'] / window_size * 30, 0, 30)
-
-    total_score = ( angle_score + wh_score + motion_score )/100 # 总分：角度+形状+持续性，总分最高100
 
     if status['cooldown_counter'] > 0:
         status['cooldown_counter'] -= 1
         return True, False ,total_score # 冷却中，仍认为是摔倒状态，但不是新事件
 
-    is_fall = angle < angle_thresh
+    score_thresh = 0.6  # 总分超过60
+    extreme_wh_ratio = 1.2  # 宽高比超过1.2
+    is_fall = angle < angle_thresh or total_score >= score_thresh or wh_ratio > extreme_wh_ratio or (height_change > 0.3)
+
     if is_fall:
         status['fall_frame_count'] += 1
         if status['fall_frame_count'] >= window_size:
